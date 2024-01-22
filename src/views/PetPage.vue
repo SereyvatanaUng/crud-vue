ad69b8272ce4ac46f4b0f5ad33556fbdc69b7ce4
SereyvatanaUng <sereyvatanau@gmail.com>
2024-01-22 11:41:33 +0700

    Display something using mapState

--- src/views/PetPage.vue
+++ src/views/PetPage.vue
@@ -1,1 +1,37 @@
-<template><div>pet</div></template>
+<template>
+  <div>
+    <h1>{{ animal.name }} ({{ $route.params.species }})</h1>
+    <p>Age: {{ animal.age }} years old</p>
+    <p>Breed: {{ animal.breed }}</p>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  data() {
+    return {
+      animal: {},
+    };
+  },
+  computed: {
+    ...mapState(["catsData", "dogsData"]),
+  },
+  watch: {
+    "$route.params": {
+      handler() {
+        this.fetchAnimal();
+      },
+      immediate: true,
+    },
+  },
+  methods: {
+    fetchAnimal() {
+      const animal =
+        this[this.$route.params.species + "Data"][this.$route.params.id];
+      this.animal = animal;
+    },
+  },
+};
+</script>
